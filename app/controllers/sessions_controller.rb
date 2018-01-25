7dfca61e17b050a7c5e2da886669cb9e9837eb0f
Pedro Souza <posouza@gmail.com>
2018-01-25 12:16:21 -0200

    Created logout session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ before_action :block_access, except: [:destroy]
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+      if @user && @user.authenticate(params[:session][:password])
         sign_in(@user)
-        redirect_to @user
-    else
-        render 'new'
-    end
+        redirect_to current_user
+      else
+        render action: :new
+      end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get    'sign_in'   => 'sessions#new'
   post   'sign_in'   => 'sessions#create'
   delete 'sign_out'  => 'sessions#destroy'
+
+  root 'sessions#new'
 end
