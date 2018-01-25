e646a0ee4f29df4a6eb9d164e8c5e47bc3dfc8ac
Pedro Souza <posouza@gmail.com>
2018-01-25 00:07:28 -0200

    Created User controller,view and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to @user, notice: "User was successfully created!"
+        #tire o método de comentário quando criar o helper.
+        #User can automatically access the system after registration.
+    #sign_in(@user)
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
