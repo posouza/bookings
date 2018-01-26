c65613b11f8a291a48004764d638514a9e212dd4
Pedro Souza <posouza@gmail.com>
2018-01-26 09:37:30 -0200

    Model completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125002444) do
+ActiveRecord::Schema.define(version: 20180126012129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "motels", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
