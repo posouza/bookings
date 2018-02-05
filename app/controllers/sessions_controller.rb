2490711e37390f674dd8c612cea710c6b8430a11
Pedro Souza <posouza@gmail.com>
2018-02-05 15:40:17 -0200

    adding a comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-#This method is to prevent the user from logging in again once it's already logged.
+#This method is to prevent the user from logging in again once it's already logged in.
 before_action :block_access, except: [:destroy]
 
   def create
